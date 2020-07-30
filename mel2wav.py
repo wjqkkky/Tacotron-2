74c2b5622f82f46849d428b637ccc1a71ca63e0b
wangjiaqi12807 <wangjiaqi12807@autohome.com.cn>
2020-07-30 15:34:14 +0800

    add G&L mel2wav

---
+++ mel2wav.py
@@ -0,0 +1,15 @@
+import os
+
+import numpy as np
+from hparams import hparams
+from datasets import audio
+
+mel_dir = "tacotron_output/eval/"
+files = os.listdir(mel_dir)
+for file in files:
+	if file[-3:] == "npy":
+		path_file = os.path.join(mel_dir, file)
+		mel_data = np.load(path_file)
+		wav = audio.inv_mel_spectrogram(mel_data.T, hparams)
+		wav_name = os.path.join("wavs/", file[:-3] + ".wav")
+		audio.save_wav(wav, wav_name, sr=22050)
